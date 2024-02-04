28b9bb1cee9597f81f0ea61b3161e17b81227c58
Tanay Mahindru <tanay.mahindru@protonmail.com>
2024-02-04 05:09:37 -0800

    card-layout-fixes

--- my-podbox/src/app/page.js
+++ my-podbox/src/app/page.js
@@ -3,7 +3,6 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import { SessionProvider, LoginButton } from "@inrupt/solid-ui-react";
 
-
 export default function Home() {
   return (
     <main className={styles.main}>
@@ -12,33 +11,37 @@ export default function Home() {
           Try to login to get started. Use one of the login buttons below. 
         </p>
         
-        <div>
-
-        </div>
       </div>
 
       <div className={styles.grid}>
         <div className={styles.card}>
           <h2>
           <SessionProvider sessionId="some-id">
+          <span>
             <LoginButton
               oidcIssuer="https://inrupt.net"
               redirectUrl="https://localhost:3000/"
-            />
-            
-        </SessionProvider><span>-&gt;</span>
+            >
+              Inrupt Login
+            </LoginButton>
+
+            -&gt;</span>
+        </SessionProvider>
           </h2>
           <p>Link POD provided by Inrupt.net</p>
         </div>
 
         <div className={styles.card}>
           <h2>
+          <span>
           <SessionProvider sessionId="some-id-2">
             <LoginButton
               oidcIssuer="https://solidcommunity.net"
               redirectUrl="https://localhost:3000/"
-            />
-          </SessionProvider><span>-&gt;</span>
+            >
+              SC Login
+            </LoginButton>
+          </SessionProvider>-&gt;</span>
           </h2>
           <p>Link POD provided by Solidcommunity.net</p>
         </div>
