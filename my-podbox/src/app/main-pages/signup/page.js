e06214fe4c8e17baae51968da85fd4c95b0fe66d
Drake White <DrakeDWhite@gmail.com>
2024-05-01 16:13:46 -0700

    added signUp pages and associated images

--- my-podbox/src/app/main-pages/signup/page.js
+++ my-podbox/src/app/main-pages/signup/page.js
@@ -4,6 +4,9 @@
 "use client";
 import React, { useEffect, useState } from 'react';
 import styles from "./page.module.css";
+import Image from 'next/image';
+import beanImage from "../../../../public/beanDiagram.svg"
+import learnMoreImage from "../../../../public/signUp_learnMore.svg"
 
 
 // things I need from pod input; app name, app image, last modified, date created, link to app, link to app controls, link to view related data 
@@ -14,11 +17,54 @@ const signup = () => {
 // html content to show on the page after we've run everything above this 
   return (
     <div className={styles.main}>
-        {/* Check if webId is null */}
-          <div>
-              {/* Will need to add placeholder and pull in user's name */}
-              <h1>Sign Up!</h1>
-          </div>
+      <div className={styles.topmatter}>
+        <div>
+            {/* Will need to add placeholder and pull in user's name */}
+            <h1>Get started with your POD for free in just one step. </h1>
+        </div>
+
+        <div className={styles.dropdownContainer}>
+          <br />
+          <select name="other providers"  className={styles.dropdown}>
+            <option value=""disabled selected hidden> Select POD Provider </option>
+            <option value="Inrupt"> Inrupt </option>
+            <option value="PodCommunity"> PodCommunity </option>
+            <option value="PodWeb"> PodWeb </option>
+            <option value="TrinPod"> TrinPod </option>
+            <option value="datapod"> DataPod </option>
+            <option value="redpencil"> RedPencil </option>
+
+          </select>
+
+          <button>
+            Register
+          </button>
+        </div>
+      </div>
+      <div className={styles.bottommatter}>
+        <div>
+          <br />
+            <br />
+            <br />
+          <h2>
+            What is a POD?
+          </h2>
+          <p>
+
+            PODs serve as your data storage space, accommodating any type of data, and are established through a POD provider. 
+            <br />
+            <br />
+            Each POD is assigned a unique WebID upon creation, a crucial identifier for future access. To obtain a WebID, register on the provider's site, note your WebID, and come back to PodBox and log in with your new POD!
+          </p>
+            <div className={styles.learnMore}>
+              <Image src={learnMoreImage} alt="" />
+            </div>
+        </div>
+        <div className={styles.diagram}>
+          
+          <Image src={beanImage} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
